07064b75f37abc2bb0caeeb8ebe8c59f1b17f6a9
acnelexh <chenzyu@umich.edu>
2022-11-20 01:16:48 -0500

    added scheduler

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 from torchvision.models import resnet18
 from torchvision.datasets import CIFAR10
 from pytorch_resnet_cifar10.resnet import resnet20, resnet32, resnet44, resnet56
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
 
 model_fns = [resnet18, resnet20, resnet32, resnet44, resnet56]
 
@@ -16,11 +18,12 @@ def _train_model_step(model, data, label, optimizer):
     return loss
 
 def _train_model(
-    model,
-    train_loader,
-    optimizer,
-    epochs=350,
-    device='cuda'):
+        model,
+        train_loader,
+        optimizer,
+        scheuler,
+        epochs=350,
+        device='cuda'):
     step_count = 0
     step_loss = []
     for e in range(epochs):
@@ -28,26 +31,35 @@ def _train_model(
             data = data.to(device)
             label = label.to(device)
             loss = _train_model_step(model, data, label, optimizer)
+            scheuler.step()
             step_loss.append(loss)
             step_count += 1
             if step_count % 100 == 0:
                 print(f'Epoch: {e}, Step: {step_count}, Loss: {loss}')
     return step_loss
     
-def train_model(epochs=350, device='cuda'):
+def train_model(
+        model,
+        train_loader,
+        lr=0.01,
+        momentum=0.9,
+        weight_decay=5e-4,
+        epochs=350,
+        T_max=200,
+        device='cuda',
+        output_file='output.csv'):
+    # main training function
     batch_size = 128
-    with open('results.csv', 'w') as f:
-        df = pd.DataFrame()
-    model_fn = model_fns[0]
-    model = model_fn()
+    df = pd.DataFrame()
+    model_fn = model_fns[1]
     model = model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-    cifar_loader = get_cifar_loader(batch_size)
-    step_loss = _train_model(model, cifar_loader, optimizer, epochs)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    scheuler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
+    step_loss = _train_model(model, train_loader, optimizer, scheuler, epochs=epochs)
     df.insert(0, 'step', range(len(step_loss)))
     step_loss_detach = [x.cpu().detach().numpy() for x in step_loss]
     df.insert(1, model_fn.__name__, step_loss_detach)
-    df.to_csv('results.csv', index=False)
+    df.to_csv(output_file, index=False)
     return
         
 
@@ -65,7 +77,16 @@ def get_cifar_loader(batch_size=128):
     return cifar_loader
 
 def main():
-    train_model(1)
+    model = model_fns[1]()
+    trainLoader = get_cifar_loader(128)
+    train_model(
+        model,
+        trainLoader,
+        lr=0.01,
+        momentum=0.9,
+        weight_decay=5e-4,
+        epochs=1,
+        T_max=200)
 
 if __name__ == "__main__":
     main()
