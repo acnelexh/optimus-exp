a45e402cdabe4286d3c03c6598f75549e4909860
acnelexh <chenzyu@umich.edu>
2022-11-22 12:41:05 -0500

    fix test/train acc

--- main.py
+++ main.py
@@ -11,25 +11,14 @@ from torchmetrics import Accuracy
 
 model_fns = [resnet18, resnet20, resnet32, resnet44, resnet56, resnet50]
 
-def _train_model_step(model, data, label, optimizer):
+def _train_model_step(model, data, label, optimizer, metrics):
     optimizer.zero_grad()
     output = model(data)
+    metrics(output, label)
     loss = torch.nn.functional.cross_entropy(output, label)
     loss.backward()
     optimizer.step()
     return loss
-
-def test_model(model, test_loader, device='cuda'):
-    metrics = Accuracy()
-    metrics.to(device)
-    model.eval()
-    for data, label in test_loader:
-        data = data.to(device)
-        label = label.to(device)
-        pred = model(data)
-        metrics(pred, label)
-    model.train()
-    return metrics.compute().cpu().detach().numpy().item()
 
 def _train_model(
         model,
@@ -41,21 +30,23 @@ def _train_model(
         test_loader=None):
     step_count = 0
     step_loss = []
+    metrics = Accuracy()
+    metrics.to(device)
     for e in range(epochs):
         for data, label in train_loader:
             data = data.to(device)
             label = label.to(device)
-            loss = _train_model_step(model, data, label, optimizer)
+            loss = _train_model_step(model, data, label, optimizer, metrics)
             scheuler.step()
             step_loss.append(loss)
             step_count += 1
             if step_count % 100 == 0:
                 print(f'Epoch: {e}, Step: {step_count}, Loss: {loss}')
-        if test_loader is not None:
-            test_acc = test_model(model, test_loader, device)
-            print(f'Epoch: {e}, Acc: {test_acc}')
-        if test_acc > 0.92:
+        train_acc = metrics.compute().cpu().detach().numpy().item()
+        if train_acc > 0.92:
             break
+        print(f'Epoch: {e}, Train Accuracy: {train_acc}')
+        metrics.reset()
     return step_loss
     
 def train_model(
@@ -68,14 +59,13 @@ def train_model(
         T_max=200,
         device='cuda',
         output_file='output.csv',
-        model_name='resnet20',
-        test_loader=None):
+        model_name='resnet20'):
     # main training function
     df = pd.DataFrame()
     model = model.to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     scheuler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
-    step_loss = _train_model(model, train_loader, optimizer, scheuler, epochs=epochs, test_loader=test_loader)
+    step_loss = _train_model(model, train_loader, optimizer, scheuler, epochs=epochs)
     df.insert(0, 'step', range(len(step_loss)))
     step_loss_detach = [x.cpu().detach().numpy() for x in step_loss]
     df.insert(1, model_name, step_loss_detach)
@@ -117,7 +107,6 @@ def get_cifar_loader(batch_size=128):
 def main():
     model = model_fns[-1]()
     trainLoader = get_cifar_loader(128)
-    testLoader = get_cifar_test_loader(128)
     train_model(
         model,
         trainLoader,
@@ -127,7 +116,6 @@ def main():
         epochs=350,
         T_max=200,
         model_name='resnet50',
-        test_loader=testLoader,
         output_file='resnet50.csv')
 
 if __name__ == "__main__":
